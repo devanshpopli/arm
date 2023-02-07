7e95c595deab511725ea49ccfb6167596f13f83b
devanshpopli <52446048+devanshpopli@users.noreply.github.com>
2023-02-07 21:29:53 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -2,6 +2,6 @@ $diskConfig1 = New-AzDiskConfig -SkuName ${Env:storageType} -Location ${Env:loca
 $dataDisk1 = New-AzDisk -DiskName ${ENV:dataDiskName} -Disk $diskConfig1 -ResourceGroupName ${ENV:rgName}
 
 $vm = Get-AzVM -Name ${ENV:vmName} -ResourceGroupName ${ENV:rgName}
-$vm = Add-AzVMDataDisk -VM $vm -Name ${ENV:dataDiskName} -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 1
+$vm = Add-AzVMDataDisk -VM $vm -Name ${ENV:dataDiskName} -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 2
 
 Update-AzVM -VM $vm -ResourceGroupName ${ENV:rgName}
